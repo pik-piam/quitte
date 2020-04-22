e606cd63927440588081afd6e788a78113688e81
Antoine Levesque <levesque@pik-potsdam.de>
2020-04-22 15:37:36 +0200

    bugfix: adapted removeColNa to the new treatment of missing data in as.quitte

--- R/removeColNa.R
+++ R/removeColNa.R
@@ -1,6 +1,6 @@
 #' Removes all NA columns of a data frame
 #'
-#' \code{removeColNa()} Removes all columns of a data frame for which all entries are NA 
+#' \code{removeColNa()} Removes all columns of a data frame for which all entries are NA, or the default of fct_explict_na 
 #'
 #' @param df a data frame
 #' @return a data frame
@@ -11,13 +11,21 @@
 #'     factor = as.factor(LETTERS[1:5]),
 #'     value = 1:5,
 #'     unit = NA,
+#'     unit2 = forcats::fct_explicit_na(factor(NA)),
 #'     stringsAsFactors = FALSE)
 #' str(df)
 #' str(removeColNa(df))
+#' @importFrom forcats fct_explicit_na
+#' 
 #' @export
 #' 
 
 removeColNa <- function(df){
+  
+  .fct_default = levels(forcats::fct_explicit_na(factor(NA)))
+  
+  df = df[colSums(!is.na(df)) > 0]
+  df = df[colSums(df != .fct_default) > 0]
 
-return(df[colSums(!is.na(df)) > 0])
+return(df)
 }
