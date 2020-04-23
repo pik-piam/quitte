548e1692d33e5f2d2ea664fb558025192b46cac5
piklev <piklev@users.noreply.github.com>
2020-04-23 08:23:23 +0200

    bugfix: adapted removeColNa to the new treatment of missing data in as.quitte (#6)

---
+++ tests/testthat/test-removeColNa.R
@@ -0,0 +1,24 @@
+context("removeColNa()")
+
+# create some test data
+df <- data.frame(
+    character = letters[1:5],
+    factor = as.factor(LETTERS[1:5]),
+    value = 1:5,
+    unit = NA,
+    unit2 = forcats::fct_explicit_na(factor(NA)),
+    stringsAsFactors = FALSE)
+
+df_removed <- removeColNa(df)
+
+test_that(
+    'Test if all NA columns disappeared',
+    {
+        # identical number of columns
+        expect_equal(
+            object   = setdiff(colnames(df), colnames(df_removed)),
+            expected = c("unit","unit2"))
+        
+       
+    }
+)
