5719748617013250d8ac9b84bc33d846cd9a3b88
Michaja Pehl <pehl@pik-potsdam.de>
2021-09-23 10:42:29 +0200

    add madrat_mule() to transfer data disguised as a MAgPIE object

---
+++ R/madrat_mule.R
@@ -0,0 +1,32 @@
+#' Madrat Mule
+#'
+#' Convert _anything_ to a [`magpie`][magclass::magclass] object and back to
+#' traffic data across those [`madrat`][madrat::madrat] borders.
+#'
+#' @md
+#' @param x Anything.
+#'
+#' @return A [`magpie`][magclass::magclass] object containing `x` (in unusable
+#'   form), or the original `x` if a [`magpie`][magclass::magclass] object was
+#'   passed.
+#'
+#' @author Michaja Pehl
+#'
+#' @examples
+#' str(x <- madrat_mule(quitte_example_data))
+#' madrat_mule(x)
+#'
+#' @importFrom magclass as.magpie
+
+#' @export
+madrat_mule <- function(x)
+{
+    if (!'magpie' %in% class(x))
+    {
+        return(as.magpie(as.integer(serialize(x, NULL))))
+    }
+    else
+    {
+        return(unserialize(as.raw(as.vector(x))))
+    }
+}
---
+++ tests/testthat/test-madrat_mule.R
@@ -0,0 +1,8 @@
+context('madrat_mule()')
+
+test_that(
+    desc = 'madrat_mule() returns identical tibbles',
+    code = expect_identical(
+        object = madrat_mule(madrat_mule(quitte_example_data)),
+        expected = quitte_example_data)
+)
