24c68c1b1700b77b22c107083a134068a5541950
Michaja Pehl <pehl@pik-potsdam.de>
2021-09-23 16:59:22 +0200

    add calc_mode() to calculate the mode of a sample

---
+++ R/calc_mode.R
@@ -0,0 +1,20 @@
+#' Calculate the mode of a sample
+#'
+#' @md
+#' @param v A vector.
+#'
+#' @return The mode, or a vector of modes if the sample is multi-modal.
+#'
+#' @author Michaja Pehl
+#'
+#' @examples
+#' calc_mode(c(1, 1, 100))
+#' calc_mode(c('a', 'a', 'b', 'c', 'c'))
+
+#' @export
+calc_mode <- function(v)
+{
+    u <- unique(v)
+    t <- tabulate(match(v, u))
+    u[which(max(t) == t)]
+}
