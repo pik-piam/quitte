219356d3de13e1367471f6ee923de18cb1d7c4ed
Johannes Koch <jokoch@pik-potsdam.de>
2022-02-03 12:06:18 +0100

    Remove plyr dependency

--- R/revalue.levels.R
+++ R/revalue.levels.R
@@ -1,12 +1,15 @@
 #' Revalue data frame factor levels.
 #'
-#' Revalue the names of a level or character column in a dataframe, according to a named vector given as an input
+#' Revalue the names of a level or character column in a dataframe, according to 
+#' a named vector given as an input.
 #'
 #' @param df A data frame (or quitte object).
-#' @param ... Name-value pairs assigning a named vector with new names to a column from the dataframe.
-#' @param dots A named list of columns containing the named vector with the old and new names for each column
+#' @param ... Named vectors of named chr, or a list of named vectors of named chr. 
+#'   The names of the vectors must correspond to column names of df. The vectors themselves
+#'   should have as names the the current values in the column to be replaced, and as values the new 
+#'   (replacement) values.
 #' @return A data frame (or quitte object, same as \code{data}).
-#' @author Antoine Levesque
+#' @author Antoine Levesque, Johannes Koch
 #' @examples
 #' 
 #' data <- inline.data.frame(c(
@@ -16,38 +19,26 @@
 #'   "REMIND;   Baseline;   CHN;      GDP per Capita|MER; US$2005/yr;   2010;     7000"))
 #'   
 #'reg_vec = c(USA = "United States")
-#'var_vec = c("GDP per Capita|MER" = "gdp",
-#'            Population = "pop")
+#'var_vec = c("GDP per Capita|MER" = "gdp", Population = "pop")
 #'
-#'revalue.levels(data,region = reg_vec)
-#'revalue.levels_(data,list(region = reg_vec, variable = var_vec))
-#'                        
-#'@export
-revalue.levels <- function(df, ...) {
-  
-  dots <- list(...)
-  
-  revalue.levels_(df, dots)
-}
-
-#' @importFrom plyr revalue
+#'revalue.levels(data, region = reg_vec)
+#'revalue.levels(data, list(region = reg_vec, variable = var_vec))
+#' 
+#' @importFrom rlang !!!                       
 #' @export
-#' @rdname revalue.levels
-revalue.levels_ <- function(df, dots) {
-  
-  # guardians
-  if (!is.data.frame(df))
-    stop('only works on data frames')
-  
-  dot.names <- names(dots)
-  
-  for (column in dot.names) {
-   
-    reval_vec = getElement(dots, column)
-      df[, column] <-revalue(getElement(df, column), replace = reval_vec)
-      
+revalue.levels <- function(df, ...) {
+  # Check is df is a data frame
+  if (!is.data.frame(df)) {
+    rlang::abort("Bad argument. Argument 'df' must be a data frame.")
   }
   
-  return(df)
+  # ... can be either a named list, or a multiple named vectors.
+  # If the second, case, transform to named list.
+  my_list <- if (...length() == 1 && is.list(..1)) ..1 else list(...)
+  
+  for (col in names(my_list)) {
+    df[, col] <- dplyr::recode(df[[col]], !!!my_list[[col]])
+  }
+  
+  df
 }
-
