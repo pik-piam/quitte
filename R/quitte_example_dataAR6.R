25ad2e3f7f11a2634a97b18429a6a6266945841a
orichters <oliverr@pik-potsdam.de>
2023-01-19 12:14:55 +0100

    add AR6 example data

---
+++ R/quitte_example_dataAR6.R
@@ -0,0 +1,17 @@
+#' @name quitte_example_dataAR6
+#'
+#' @title quitte example data with three models (REMIND, GCAM, MESSAGEix) and two
+#' scenarios (Current Policies, Delayed transition)
+#'
+#' @description A quitte object with some example data from NGFS Phase 3.
+#'
+#' @docType data
+#'
+#' @usage quitte_example_dataAR6
+#'
+#' @author Oliver Richters
+
+NULL
+
+# quitte_example_dataAR6 <- as.quitte("/p/projects/remind/debugging/NGFS_example_data.xlsx")
+# usethis::use_data(quitte_example_dataAR6, overwrite = TRUE)
