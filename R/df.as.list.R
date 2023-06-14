2ae0ca5041a7749c8b334c54b9d7aaaa425921ce
Michaja Pehl <pehl@pik-potsdam.de>
2023-06-14 09:27:03 +0200

    fix typo in df.as.list() documentation; include importFrom

--- R/df.as.list.R
+++ R/df.as.list.R
@@ -2,11 +2,13 @@
 #'
 #' @param df A data frame.
 #' @param names Index used for naming list items.  Integer or character, must
-#'     work with `df[[name]]`.  Defaults to the first data frame column.
+#'     work with `df[[names]]`.  Defaults to the first data frame column.
 #' @param x Index used for list items.  Integer or character, must work with
 #'     `df[[x]]`.  Defaults to the second data frame column.
 #'
 #' @return A list.
+#'
+#' @importFrom stats setNames
 #'
 #' @examples
 #' (df <- data.frame(
@@ -24,4 +26,3 @@ df.as.list <- function(df, names = 1, x = 2)
 {
   setNames(as.list(df[[x]]), df[[names]])
 }
-
