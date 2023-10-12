87359db2788b1aa3722b65573bea112dd3b04277
Michaja Pehl <pehl@pik-potsdam.de>
2023-10-12 10:13:45 +0200

    check column existence

--- R/is_quitte.R
+++ R/is_quitte.R
@@ -6,5 +6,25 @@ is_quitte <- function(x, verbose = FALSE) {
                                  'unit'     = c('character', 'factor'),
                                  'period'   = c('integer', 'POSIXct'),
                                  'value'    = 'numeric')
-        return(TRUE)
+
+        is_quitte <- TRUE
+
+        # check column existence ----
+        if (any(missing_columns <- !names(required_columns) %in% colnames(x))) {
+            if (isTRUE(verbose)) {
+                message('Column', ifelse(sum(missing_columns) - 1, 's ', ' '),
+                        paste(paste0('`',
+                                     names(required_columns)[missing_columns],
+                                     '`'),
+                              collapse = ', '),
+                        ifelse(sum(missing_columns) - 1, ' are ', ' is '),
+                        'missing in `x`')
+                is_quitte <- FALSE
+            }
+            else {
+                return(FALSE)
+            }
+        }
+
+        return(is_quitte)
 }
