bbb7e14753b221664144d9d6827514757f5509da
Michaja Pehl <pehl@pik-potsdam.de>
2023-10-12 11:14:42 +0200

    check data frame

--- R/is_quitte.R
+++ R/is_quitte.R
@@ -1,33 +1,35 @@
 is_quitte <- function(x, verbose = FALSE) {
-        required_columns <- list('model'    = c('character', 'factor'),
-                                 'scenario' = c('character', 'factor'),
-                                 'region'   = c('character', 'factor'),
-                                 'variable' = c('character', 'factor'),
-                                 'unit'     = c('character', 'factor'),
-                                 'period'   = c('integer', 'POSIXct'),
-                                 'value'    = 'numeric')
-
-        .is_quitte <- TRUE
-
-        # check column existence ----
-        missing_columns <- !names(required_columns) %in% colnames(x)
-        if (any(missing_columns)) {
-            if (isTRUE(verbose)) {
-                message(
-                    'Column', ifelse(sum(missing_columns) - 1, 's ', ' '),
-                    paste(paste0('`',
-                                 names(required_columns)[missing_columns],
-                                 '`'),
-                          collapse = ', '),
-                    ifelse(sum(missing_columns) > 1, ' are ', ' is '),
-                    'missing in `x`')
-
-                .is_quitte <- FALSE
-            }
-            else {
-                return(FALSE)
-            }
+    if (!is.data.frame(x)) {
+        if (isTRUE(verbose)) {
+            message('`x` is not a data frame.')
+            return(invisible(FALSE))
         }
+        else {
+            return(FALSE)
+        }
+    }
+
+    required_columns <- list('model'    = c('character', 'factor'),
+                             'scenario' = c('character', 'factor'),
+                             'region'   = c('character', 'factor'),
+                             'variable' = c('character', 'factor'),
+                             'unit'     = c('character', 'factor'),
+                             'period'   = c('integer', 'POSIXct'),
+                             'value'    = 'numeric')
+
+    .is_quitte <- TRUE
+
+    # check column existence ----
+    missing_columns <- !names(required_columns) %in% colnames(x)
+    if (any(missing_columns)) {
+        if (isTRUE(verbose)) {
+            message(
+                'Column', ifelse(sum(missing_columns) - 1, 's ', ' '),
+                paste(
+                    paste0('`', names(required_columns)[missing_columns], '`'),
+                    collapse = ', '),
+                ifelse(sum(missing_columns) > 1, ' are ', ' is '),
+                'missing in `x`')
 
         # check column classes ----
         column_classes <- x %>%
