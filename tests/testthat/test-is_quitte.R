247511ceebd57a3a184d12f329006ba977765ddf
Michaja Pehl <pehl@pik-potsdam.de>
2023-10-12 10:27:41 +0200

    test wrong column classes

--- tests/testthat/test-is_quitte.R
+++ tests/testthat/test-is_quitte.R
@@ -1,8 +1,30 @@
+# test column existence ----
 test_that(
   desc = 'is_quitte() returns FALSE on missing columns',
   code = {
-    for (x in colnames(quitte_example_data))
+    for (col in colnames(quitte_example_data))
       expect_false(
-        is_quitte(select(quitte_example_data, -all_of(x))))
+        is_quitte(select(quitte_example_data, -all_of(col))))
+  }
+)
+
+# test wrong column classes ----
+test_that(
+  desc = 'is_quitte() returns FALSE on wrong column classes',
+  code = {
+    x <- quitte_example_data %>%
+      factor.data.frame()
+    for (col in colnames(quitte_example_data)) {
+      if (col %in% c('model', 'scenario', 'region', 'variable', 'unit')) {
+        expect_false(
+          is_quitte(mutate(x, !!sym(col) := as.integer(.data[[col]]))))
+      }
+      else if ('period' == col) {
+        expect_false(is_quitte(mutate(x, period = is.numeric(x[['period']]))))
+      }
+      else {
+        expect_false(is_quitte(mutate(x, value = as.character(x[['value']]))))
+      }
+    }
   }
 )
