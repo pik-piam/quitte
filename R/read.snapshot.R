be371014b0c12a70f5f6698464c38c2dfed0825f
orichters <oliverr@pik-potsdam.de>
2023-10-19 12:57:44 +0200

    rename to read.snapshot, refactor, make tests dependent on existence of commands

--- R/read.snapshot.R
+++ R/read.snapshot.R
@@ -1,6 +1,7 @@
 #' Reads IAMC-style .csv files obtained as a IIASA snapshot into a quitte data frame,
 #' filtering the data. This function is helpful if the csv file is large and R runs out
-#' of memory loading it completely.
+#' of memory loading it completely. This function requires head, tail and grep on your system.
+#' If not supported, use read.quitte().
 #'
 #' @md
 #' @param file Path of single IAMC-style .csv/.mif file
@@ -19,7 +20,7 @@
 #'
 #' @export
 
-read.filter.snapshot <- function(file, keep = list()) {
+read.snapshot <- function(file, keep = list()) {
   unknowntype <- setdiff(names(keep), c("model", "scenario", "region", "variable", "unit", "period"))
   if (length(unknowntype) > 0) {
     stop("Unknown types to be kept: ", toString(unknowntype))
@@ -29,19 +30,24 @@ read.filter.snapshot <- function(file, keep = list()) {
   if (length(setdiff(names(keep), "period")) > 0) {
     # always keep first lines of original file (comments, colnames), grep in the rest
     alwayskeep <- 20
-    system(paste("head -n", alwayskeep, file, ">", tmpfile))
+    exitCode <- system(paste("head -n", alwayskeep, file, ">", tmpfile))
+    if (exitCode != 0) stop("'head' command failed. Please use 'read.quitte()'")
     # the goal of the next lines is to grep one after the other through the elements of keep
     # keep = list(variable = "GDP|PPP", region = c("World", "FRA")) should get you
     # | grep -E '(GDP\|PPP)' | grep -E '(World|FRA)'
     # 1. escape | in variable names and do not grep for period
-    keepescaped <- lapply(keep[setdiff(names(keep), "period")], function(x) gsub("|", "\\|", x, fixed = TRUE))
+    cleanup <- function(x) {
+      x <- gsub("[^A-Za-z0-9\\| ]", ".", x)
+      x <- gsub("|", "\\|", x, fixed = TRUE)        
+    }
+    keepescaped <- lapply(keep[setdiff(names(keep), "period")], cleanup)
     # 2. collapse each element with a |
     keepcollapsed <- unlist(lapply(keepescaped, paste0, collapse = "|"))
     # generate a grep -E statement for each element of keep list
     greptext <- paste0(" | grep -E '(", keepcollapsed, ")'", collapse = "")
     command <- paste0("tail -n +", (alwayskeep + 1), " ", file, greptext, " >> ", tmpfile)
-    # message(command)
-    system(command)
+    exitCode <- system(command)
+    if (exitCode != 0) stop("'tail' or 'grep' command created problems. Please use 'read.quitte()'")
   } else {
     file.copy(file, tmpfile, overwrite = TRUE)
   }
