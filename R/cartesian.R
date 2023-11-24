3048810752a1445e4bc861e49c4c1147686e5e7c
Michaja Pehl <pehl@pik-potsdam.de>
2023-11-24 16:10:40 +0100

    extend cartesian() to arbitrary number of parameters

--- R/cartesian.R
+++ R/cartesian.R
@@ -1,19 +1,32 @@
 #' Generate cartesian product from to character vectors
 #'
-#' @param x,y objects that can be coerced to character vectors
-#' @param sep a character string that will seperate the elements of `x`
-#'    and `y` in the output. Defaults to `'.'`.
+#' @param ... objects that can be coerced to character vectors
+#' @param sep a character string that will separate the elements of `...` in the
+#'     output.  Defaults to `'.'`.
 #'
-#' @return A character vector of the concatenated elements of `x` and
-#'    `y`.
+#' @return A character vector of the concatenated elements of `...`.
 #'
 #' @examples
-#' cartesian(c('a', 'b'), 1:3)
-#' # [1] "a.1" "a.2" "a.3" "b.1" "b.2" "b.3"
+#' cartesian(c('a', 'b'), 1:3, c('X', 'Y', 'Z'))
+#' #  [1] "a.1.X" "b.2.Y" "a.3.Z" "b.1.X" "a.2.Y" "b.3.Z" "a.1.X" "b.2.Y"
+#' #  [9] "a.3.Z" "b.1.X" "a.2.Y" "b.3.Z" "a.1.X" "b.2.Y" "a.3.Z" "b.1.X"
+#' # [17] "a.2.Y" "b.3.Z"
 #'
 #' @export
-cartesian <- function(x, y, sep = '.') {
-    x <- as.character(x)
-    y <- as.character(y)
-    paste(rep(x, each = length(y)), rep(y, times = length(x)), sep = sep)
+cartesian <- function(..., sep = '.') {
+    dots <- list(...)
+
+    reps <- prod(lengths(dots)) / lengths(dots)
+
+    apply(
+        X = sapply(
+            X = seq_along(dots),
+            FUN = function(i) {
+                rep(dots[[i]], times = reps[[i]])
+            }
+        ),
+        MARGIN = 1,
+        FUN = paste,
+        collapse = '.'
+    )
 }
