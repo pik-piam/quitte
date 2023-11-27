6f025457d966e98ffa179703f58b17429963a1cc
Michaja Pehl <pehl@pik-potsdam.de>
2023-11-27 13:51:38 +0100

    fix cartesian()

--- R/cartesian.R
+++ R/cartesian.R
@@ -8,21 +8,27 @@
 #'
 #' @examples
 #' cartesian(c('a', 'b'), 1:3, c('X', 'Y', 'Z'))
-#' #  [1] "a.1.X" "b.2.Y" "a.3.Z" "b.1.X" "a.2.Y" "b.3.Z" "a.1.X" "b.2.Y"
-#' #  [9] "a.3.Z" "b.1.X" "a.2.Y" "b.3.Z" "a.1.X" "b.2.Y" "a.3.Z" "b.1.X"
-#' # [17] "a.2.Y" "b.3.Z"
+#' #  [1] "a.1.X" "a.1.Y" "a.1.Z" "a.2.X" "a.2.Y" "a.2.Z" "a.3.X" "a.3.Y"
+#' #  [9] "a.3.Z" "b.1.X" "b.1.Y" "b.1.Z" "b.2.X" "b.2.Y" "b.2.Z" "b.3.X"
+#' # [17] "b.3.Y" "b.3.Z"
 #'
 #' @export
 cartesian <- function(..., sep = '.') {
     dots <- list(...)
 
-    reps <- prod(lengths(dots)) / lengths(dots)
+    L <- lengths(dots)
 
     apply(
         X = sapply(
             X = seq_along(dots),
             FUN = function(i) {
-                rep(dots[[i]], times = reps[[i]])
+                # lengths of dots elements before i (excluding)
+                times_slice <- L[seq_len(i - 1)]
+                # lengths of dot elements after i (excluding)
+                each_slice  <- L[setdiff(seq_along(dots), seq_len(i))]
+
+                rep(rep(dots[[i]], times = prod(times_slice)),
+                    each = prod(each_slice))
             }
         ),
         MARGIN = 1,
