2f634dcff16a4ee4b98f63be60f55d2aad3cb671
Michaja Pehl <pehl@pik-potsdam.de>
2024-04-25 09:07:48 +0200

    reformat test-write.mif.R to use test descriptions as file outline

--- tests/testthat/test-write.mif.R
+++ tests/testthat/test-write.mif.R
@@ -1,6 +1,5 @@
 context('write.mif()')
 
-# write .mif files ----
 test_that(
   'write .mif files',
   {
@@ -20,10 +19,9 @@ test_that(
       )
   })
 
-# write .mif file with comment header ----
 test_that(
-  desc = 'write .mif file with comment header',
-  code = {
+  'write .mif file with comment header',
+  {
     f <- tempfile()
     set.seed(0)
     quitte_example_data %>%
@@ -36,10 +34,9 @@ test_that(
                                         package = 'quitte')))
   })
 
-# expect warning for empty data
 test_that(
-  desc = 'write .mif file passing empty data',
-  code = {
+  'write .mif file passing empty data',
+  {
     f <- tempfile()
     expect_warning(write.mif(filter(quitte_example_data, model == "x"), f),
                    "Writing empty data frame")
