1e4ec2b497eac6b088b6c39576b362a7a3ffec86
Michaja Pehl <pehl@pik-potsdam.de>
2024-06-14 15:14:13 +0200

    extend cartesian() to combine arbitray objects, not just character strings

--- R/cartesian.R
+++ R/cartesian.R
@@ -1,10 +1,13 @@
-#' Generate Cartesian product from character vectors
+#' Generate Cartesian Product from Vectors
 #'
-#' @param ... objects that can be coerced to character vectors
-#' @param sep a character string that will separate the elements of `...` in the
-#'     output.  Defaults to `'.'`.
+#' @param ... Vectors to be combined
+#' @param sep If a character string, that string will separate the elements of
+#'     `...` (if necessary cast to `character`) in the output.  If `NULL`,
+#'     elements of `...` will not be cast.  Defaults to `'.'`.
 #'
-#' @return A character vector of the concatenated elements of `...`.
+#' @return If `sep` is a character string, then a character vector of
+#'     concatenated elements.  If `sep` is `NULL`, then a list of concatenated
+#'     elements.
 #'
 #' @examples
 #' cartesian(c('a', 'b'), 1:3, c('X', 'Y', 'Z'))
@@ -12,16 +15,46 @@
 #' #  [9] "a.3.Z" "b.1.X" "b.1.Y" "b.1.Z" "b.2.X" "b.2.Y" "b.2.Z" "b.3.X"
 #' # [17] "b.3.Y" "b.3.Z"
 #'
+#' str(cartesian(c('a', 'b'), 17:19, sep = NULL))
+#' # List of 6
+#' #  $ :List of 2
+#' #   ..$ : chr "a"
+#' #   ..$ : int 17
+#' #  $ :List of 2
+#' #   ..$ : chr "a"
+#' #   ..$ : int 18
+#' #  $ :List of 2
+#' #   ..$ : chr "a"
+#' #   ..$ : int 19
+#' #  $ :List of 2
+#' #   ..$ : chr "b"
+#' #   ..$ : int 17
+#' #  $ :List of 2
+#' #   ..$ : chr "b"
+#' #   ..$ : int 18
+#' #  $ :List of 2
+#' #   ..$ : chr "b"
+#' #   ..$ : int 19
+
 #' @export
-cartesian <- function(..., sep = '.') {
+cartesian <- function(..., sep = '.')
+{
+    stopifnot('`sep` must be a character string or NULL'
+              = is.null(sep) || is.character(sep))
+
     if (0 == length(dots <- list(...)))
         return(NULL)
 
-    v <- as.character(dots[[1]])
+    dots <- lapply(dots, as.list)
+
+    v <- lapply(dots[[1]], as.list)
     for (i in dots[-1])
-        v <- paste(rep(v, each = length(i)),
-                   rep(as.character(i), times = length(v)),
-                   sep = sep)
+        v <- mapply(c, rep(v, each = length(i)), rep(i, times = length(v)),
+                    SIMPLIFY = FALSE, USE.NAMES = FALSE)
+
+    if (is.character(sep))
+        v <- vapply(v, paste, collapse = sep, FUN.VALUE = character(1))
 
     return(v)
 }
+
--- tests/testthat/test-cartesian.R
+++ tests/testthat/test-cartesian.R
@@ -24,3 +24,12 @@ test_that(
     {
         expect_null(object = cartesian())
     })
+
+test_that(
+    'cartesian(sep = NULL) returns list',
+    {
+        expect_equal(
+            object = cartesian(c('a', 'b'), 17:19, sep = NULL),
+            expected = list(list("a", 17L), list("a", 18L), list("a", 19L),
+                            list("b", 17L), list("b", 18L), list("b", 19L)))
+    })
