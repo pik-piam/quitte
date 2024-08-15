1181f65e4134acb6a71289204f1ecf20efc7d30f
orichters <oliverr@pik-potsdam.de>
2024-08-15 18:25:43 +0200

    export reportDuplicates() and improve docu

--- R/reportDuplicates.R
+++ R/reportDuplicates.R
@@ -1,8 +1,10 @@
-#' Finds duplicates in a quitte object and warns about them.
-#' It removes any |+| notation and also warns if units or value differ
+#' Finds duplicates in a quitte object and warns about them per model and scenario.
+#' Also warn if variables are identical but value or units differ.
 
-#' @param mifdata object that can be converted with as.quitte into quitte data.frame
+#' @param mifdata object that can be converted with as.quitte
 #' @importFrom dplyr filter mutate select
+#' @return only the data that is duplicated. Has 0 rows if everything is fine
+#' @export
 reportDuplicates <- function(mifdata) {
   mifdata <- as.quitte(mifdata)
   duplicates <- mifdata %>% 
