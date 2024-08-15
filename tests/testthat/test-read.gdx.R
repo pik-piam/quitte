26c2e55f14abc55e788545e13de8d4e82443779e
Michaja Pehl <pehl@pik-potsdam.de>
2024-08-15 11:46:07 +0200

    add test-read.gdx.R

---
+++ tests/testthat/test-read.gdx.R
@@ -0,0 +1,213 @@
+# define tests ----
+# all these tests a wrapped in a function so that they can be run with different
+# options below
+`test-read.gdx` <- function()
+{
+    gdx <- test_path('test_data/test.gdx')
+
+    info <- paste('`quitte_force_gamstransfer` =',
+                  options('quitte_force_gamstransfer'))
+
+    identical_tibble <- function(x, y)
+    {
+        a <- all.equal(x, y)
+        b <- all.equal(attributes(x)[sort(names(attributes(x)))],
+                       attributes(y)[sort(names(attributes(y)))])
+
+        if (!isTRUE(a))
+        {
+            return(a)
+        }
+        else if (!isTRUE(b))
+        {
+            return(b)
+        }
+        else
+        {
+            return(TRUE)
+        }
+    }
+
+    ## sets ----
+    test_that(
+        'read.gdx() reads sets correctly',
+        {
+            x <- read.gdx(gdx, 'set_d1_lower')
+
+            expect_true(tibble::is_tibble(x), info = info)
+            expect_identical('foo', colnames(x), info = info)
+            expect_identical(c('foo' = 'character'), sapply(x, class),
+                             info = info)
+            expect_equal(length(set_d1_lower), nrow(x), info = info)
+        })
+
+    test_that(
+        'read.gdx() reas two-dimensional sets correctly',
+        {
+            x <- read.gdx(gdx, 'set_d2_alias')
+
+            expect_true(tibble::is_tibble(x), info = info)
+
+            expect_identical(c('set_d1_UPPER', 'set_d1_lower'), colnames(x),
+                             info = info)
+
+            expect_identical(
+                sapply(x, class),
+                c('set_d1_UPPER' = 'character', 'set_d1_lower' = 'character'),
+                info = info)
+
+            expect_equal(prod(lengths(list(set_d1_UPPER, set_d1_lower))),
+                         nrow(x),
+                         info = info)
+        })
+
+    test_that(
+        'read.gdx() reads set aliases correctly',
+        {
+            x <- read.gdx(gdx, 'set_d2')
+            y <- read.gdx(gdx, 'set_d2_alias')
+
+            expect_identical(x, y, info = info)
+        })
+
+    ## parameters ----
+    test_that(
+        'read.gdx() reads scalars correctly',
+        {
+            x <- read.gdx(gdx, 'parameter_d0')
+
+            expect_equal(object = x,
+                         expected = c('parameter_d0' = parameter_d0),
+                         info = info)
+        })
+
+    test_that(
+        'read.gdx() reads parameters correctly',
+        {
+            x <- read.gdx(gdx, 'parameter_d1')
+
+            expect_true(identical_tibble(x, parameter_d1), info = info)
+        })
+
+    test_that(
+        'read.gdx() reads two-dimensional parameters correctly',
+        {
+            x <- read.gdx(gdx, 'parameter_d2')
+
+            expect_true(identical_tibble(x, parameter_d2), info = info)
+        })
+
+    ## variables ----
+    test_that(
+        'reaf.gdx() reads scalar variables correctly',
+        {
+            x <- read.gdx(gdx, 'variable_d0')
+
+            expect_equal(x,
+                         c('variable_d0' = variable_d0[['level']]),
+                         info = info)
+        })
+
+    test_that(
+        'read.gdx() reads one-dimensional variables correctly',
+        {
+            x <- read.gdx(gdx, 'variable_d1')
+
+            expect_true(
+                identical_tibble(
+                    x,
+                    variable_d1 %>%
+                        select('set_d1_UPPER', 'value' = 'level')),
+                info = info)
+
+            x <- read.gdx(gdx, 'variable_d1',
+                          fields = c('l', 'm', 'lo', 'up', 's'),
+                          colNames = c('set_d1_UPPER', 'level', 'marginal',
+                                       'lower', 'upper', 'scale'),
+                          squeeze = FALSE)
+
+            expect_true(identical_tibble(x, variable_d1), info = info)
+        })
+
+    test_that(
+        'read.gdx() reads two-dimensional variables correctly',
+        {
+            x <- read.gdx(gdx, 'variable_d2')
+
+            expect_true(
+                identical_tibble(x,
+                                 variable_d2 %>%
+                                     select('set_d1_UPPER', 'set_d1_lower',
+                                            'value' = 'level')),
+                info = info)
+
+            x <- read.gdx(gdx, 'variable_d2',
+                          fields = c('l', 'm', 'lo', 'up', 's'),
+                          colNames = c('set_d1_UPPER', 'set_d1_lower',
+                                       'level', 'marginal', 'lower', 'upper',
+                                       'scale'),
+                          squeeze = FALSE)
+
+            expect_true(identical_tibble(x, variable_d2), info = info)
+        })
+
+    ## equations ----
+    test_that(
+        'read.gdx() reads scalar equations correctly',
+        {
+            x <- read.gdx(gdx, 'equation_d0')
+
+            expect_identical(x,
+                             c('equation_d0' = equation_d0[['level']]),
+                             info = info)
+        })
+
+    test_that(
+        'read.gdx() reads one-dimensional equations correctly',
+        {
+            x <- read.gdx(gdx, 'equation_d1')
+
+            expect_true(
+                identical_tibble(
+                    x,
+                    equation_d1 %>%
+                        select('set_d1_UPPER', 'value' = 'level')),
+                info = info)
+
+            x <- read.gdx(gdx, 'equation_d1',
+                          fields = c('l', 'm', 'lo', 'up', 's'),
+                          colNames = c('set_d1_UPPER', 'level', 'marginal',
+                                       'lower', 'upper', 'scale'),
+                          squeeze = FALSE)
+
+            expect_true(identical_tibble(x, equation_d1), info = info)
+        })
+
+    test_that(
+        'read.gdx() reads two-dimensional equations correctly',
+        {
+            x <- read.gdx(gdx, 'equation_d2')
+
+            expect_true(
+                identical_tibble(x,
+                                 equation_d2 %>%
+                                     select('set_d1_UPPER', 'set_d1_lower',
+                                            'value' = 'level')),
+                info = info)
+
+            x <- read.gdx(gdx, 'equation_d2',
+                          fields = c('l', 'm', 'lo', 'up', 's'),
+                          colNames = c('set_d1_UPPER', 'set_d1_lower',
+                                       'level', 'marginal', 'lower', 'upper',
+                                       'scale'),
+                          squeeze = FALSE)
+
+            expect_true(identical_tibble(x, equation_d2), info = info)
+        })
+}
+
+# run tests ----
+# set up test data to compare to
+source(test_path('test_data/make_test_data.R'))
+
+`test-read.gdx`()
---
+++ tests/testthat/test_data/make_test_data.R
@@ -0,0 +1,55 @@
+library(dplyr)
+library(tidyr)
+
+# sets ----
+set_d1_lower <- letters[23:26]
+set_d1_UPPER <- LETTERS[1:3]
+set_d2 <- expand_grid(set_d1_UPPER, set_d1_lower)
+
+# parameters ----
+parameter_d0 <- 13
+
+parameter_d1 <- tibble(set_d1_UPPER) %>%
+    mutate(value = 1:n())
+
+parameter_d2 <- set_d2 %>%
+    mutate(
+        value = as.integer((Vectorize(charToRaw))(set_d1_UPPER)) * 10000
+              + as.integer((Vectorize(charToRaw))(set_d1_lower)))
+
+# variables ----
+variable_d0 <- data.frame('level'    =  13,
+                          'marginal' =  -1,
+                          'lower'    =   0,
+                          'upper'    = Inf,
+                          'scale'    =  10)
+
+variable_d1 <- expand_grid(set_d1_UPPER,
+                           variable_d0 %>%
+                               pivot_longer(everything())) %>%
+    mutate(value = value
+                 * ( as.integer((Vectorize(charToRaw))(set_d1_UPPER))
+                   - as.integer(charToRaw(set_d1_UPPER[1]))
+                   + 1)) %>%
+    pivot_wider()
+
+variable_d2 <- expand_grid(set_d2,
+                           variable_d0 %>%
+                               pivot_longer(everything())) %>%
+    mutate(value = value
+                 * ( ( as.integer((Vectorize(charToRaw))(set_d1_UPPER))
+                     - as.integer(charToRaw(set_d1_UPPER[1]))
+                     + 1
+                     )
+                   * 10000
+                   + ( as.integer((Vectorize(charToRaw))(set_d1_lower))
+                     - as.integer(charToRaw(set_d1_lower[1]))
+                     + 1
+                     )
+                   )) %>%
+    pivot_wider()
+
+# equations ----
+equation_d0 <- variable_d0
+equation_d1 <- variable_d1
+equation_d2 <- variable_d2
