12b67fdd8a9f18ca9f1621d4396368476a2173a0
Michaja Pehl <pehl@pik-potsdam.de>
2024-12-05 11:42:10 +0100

    add signif_frac()

---
+++ R/signif_frac.R
@@ -0,0 +1,30 @@
+#' Round fractions only
+#'
+#' Round numbers to significant digits, preserving integer parts.
+#'
+#' @md
+#' @param x A numeric vector.
+#' @param digits Number of significant digits, defaults to 3.
+#'
+#' @return A numeric vector.
+#'
+#' @examples
+#' set.seed(0)
+#' x <- runif(8, -1, 1) * 10 ^ seq.int(5, -2)
+#'
+#' data.frame(number           = x,
+#'            formatted        = sprintf('%g', x),
+#'            `w/ signif`      = sprintf('%g', signif(x, 3)),
+#'            `w/ signif_frac` = sprintf('%g', signif_frac(x)),
+#'            check.names = FALSE)
+#'
+#' @importFrom glue glue
+
+#' @export
+signif_frac <- function(x, digits = 3)
+{
+    if (!is.numeric(x))
+        stop(glue('x must be numeric, but is {class(x)}.'))
+
+    sapply(x, \(x) signif(x, max(digits, ceiling(log10(abs(x))))))
+}
