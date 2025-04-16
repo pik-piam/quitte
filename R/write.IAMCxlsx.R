75d24cf9ecf3aac2f18cfb7b373edbfdeee47f84
Tonn Rüter <tonnru@pik-potsdam.de>
2025-04-16 07:49:40 +0200

    Convert quitte to wide IAMC format
    Introduced `as.IAMCTimeseries` to convert quitte data frame from long to wide format, so that years/periods become additional columns

--- R/write.IAMCxlsx.R
+++ R/write.IAMCxlsx.R
@@ -14,34 +14,19 @@
 #' @examples
 #' write.IAMCxlsx(quitte_example_data, tempfile())
 #'
-#' @importFrom dplyr mutate rename_with select
-#' @importFrom tidyr pivot_wider
-#' @importFrom tidyselect matches
-#' @importFrom stringr str_to_title
 #' @importFrom writexl write_xlsx
-
 #' @export
-write.IAMCxlsx <- function(x, path, append = FALSE) {
+write.IAMCxlsx <- function(x, path, append = FALSE, is.IAMCTimeseries = FALSE) {
     x <- as.quitte(x)
     if (nrow(x) == 0) warning("Writing empty data frame to ", path)
-    default_columns <- c('Model', 'Scenario', 'Region', 'Variable', 'Unit',
-                         'Period', 'Value')
 
-    . <- NULL
+    # convert to IAMC timeseries format
+    if (!is.IAMCTimeseries)
+        x <- as.IAMCTimeseries(df = x, fill = NA)
 
     if (append && file.exists(path)) {
         x <- rbind(read.quitte(path), x)
     }
-
-    # make column names upper-case
-    x <- x %>%
-        rename_with(.fn = str_to_title,
-                    .cols = matches('^[A-Za-z][A-Za-z0-9_]*$')) %>%
-        select(setdiff(default_columns, last(default_columns)),
-               setdiff(colnames(.), default_columns),
-               last(default_columns)) %>%
-               filter(is.finite(.data$Value), '' != .data$Value) %>%
-               pivot_wider(names_from = 'Period', names_sort = TRUE, values_from = 'Value', values_fill = NA)
 
     write_xlsx(list("data" = x), path)
 }
