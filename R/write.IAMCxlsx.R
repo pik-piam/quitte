68243c1368cf0f622136abee7faace826598703f
Tonn Rüter <tonnru@pik-potsdam.de>
2025-04-17 13:57:19 +0200

    Offload pivoting to wide format to ax.IAMCTimeseries
    Also introduced parameter is.IAMCTimeseries that indicates whether the provided data frame already is in wide format

--- R/write.IAMCxlsx.R
+++ R/write.IAMCxlsx.R
@@ -8,6 +8,8 @@
 #' @param path Path or connection to write to.
 #' @param append Overwrite existing files (`FALSE`, default), or append to them
 #'   (`TRUE`).
+#' @param is.IAMCTimeseries Set TRUE if data frame has already been converted to
+#'  wide format. Defaults to FALSE.
 #'
 #' @author Michaja Pehl, Oliver Richters
 #'
@@ -20,13 +22,20 @@ write.IAMCxlsx <- function(x, path, append = FALSE, is.IAMCTimeseries = FALSE) {
     x <- as.quitte(x)
     if (nrow(x) == 0) warning("Writing empty data frame to ", path)
 
+    if (append && file.exists(path)) {
+        other <- read.quitte(path)
+        # If original df is already in wide format, we need to cast other as well
+        if (is.IAMCTimeseries)
+            other <- other %>% as.IAMCTimeseries()
+        extra_cols <- setdiff(colnames(other), colnames(x))
+        if(length(extra_cols) > 0)
+            stop("Cannot append to file ", path, " as it contains extra columns: ", paste(extra_cols, collapse = ", "))
+        x <- rbind(other, x)
+    }
+
     # convert to IAMC timeseries format
     if (!is.IAMCTimeseries)
         x <- as.IAMCTimeseries(df = x, fill = NA)
-
-    if (append && file.exists(path)) {
-        x <- rbind(read.quitte(path), x)
-    }
 
     write_xlsx(list("data" = x), path)
 }
