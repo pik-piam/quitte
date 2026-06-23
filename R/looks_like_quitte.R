d204f5e43c7d2e59aea180e3ca1a9962c317a419
Michaja Pehl <pehl@pik-potsdam.de>
2026-06-23 11:16:55 +0200

    modify looks_like_quitte message format
    - Use cli_abort, cli_warn, or cli_inform.
    - Follow Tidyverse style guide on error messages.

--- R/looks_like_quitte.R
+++ R/looks_like_quitte.R
@@ -8,9 +8,10 @@
 #' - The "value" column must be of type "numeric".
 #'
 #' @param df An object to test for `quitte` structure.
-#' @param verbose Any function like [`message()`][base::message],
-#'     [`warning()`][base::warning], or [`stop()`][base::stop] to report if any
-#'     of the criteria of the quitte definition are not fulfilled.
+#' @param verbose Either `NULL`, or one of `'abort'`, `'warn'`, or `'inform`',
+#'     to report if any of the criteria of the quitte definition are not
+#'     fulfilled using `[cli::cli_abort]`, `[cli::cli_warn]`, or
+#'     `[cli::cli_inform]`.
 #'
 #' @returns `TRUE` if all criteria are met, `FALSE` otherwise.
 #'
@@ -23,50 +24,67 @@
 #' quitte_example_data %>%
 #'     select(-'model') %>%
 #'     mutate(period = as.character(period)) %>%
-#'     looks_like_quitte(verbose = message)
+#'     looks_like_quitte(verbose = 'inform')
 #'
-#' @importFrom cli pluralize
+#' @importFrom cli cli_abort cli_warn cli_inform
 
 #' @export
 looks_like_quitte <- function(df, verbose = NULL)
 {
     if (is.null(verbose)) {
-        verbose <- \(...) invisible()
+        verbose <- \(...) {}
+    } else if (isTRUE(verbose == 'abort')) {
+        verbose <- cli_abort
+    } else if (isTRUE(verbose == 'warn')) {
+        verbose <- cli_warn
+    } else if (isTRUE(verbose == 'inform')) {
+        verbose <- cli_inform
+    } else {
+        cli_abort(c(
+             paste('{.arg verbose} must be one of {.val abort}, {.val warn},',
+                   '{.val inform}, or {.code NULL}.'),
+            i = 'It is {.val {verbose}}.'))
     }
 
+    msg <- character(0)
+
     if (!is.data.frame(df)) {
-        verbose('`df` must be a data frame.')
+        msg <- c(msg,
+                 '{.arg df} must be a data frame, not {.obj_type_friendly df}.')
         return(FALSE)
     }
 
     looks_like_quitte <- TRUE
 
-    if (length(missing_columns <- setdiff(c('model', 'scenario', 'region',
-                                            'variable', 'unit', 'period',
-                                            'value'), colnames(df)))) {
-        missing_columns <- paste0('`', missing_columns, '`')
-        verbose(pluralize('Mandatory columns{?s} {missing_columns} missing ',
-                          'from `df`.'))
+    mandatory_columns <- c('model', 'scenario', 'region', 'variable', 'unit',
+                           'period', 'value')
+    is_missing <- !(mandatory_columns %in% colnames(df))
+    if (any(is_missing)) {
+        msg <- c(msg,
+                 '{.arg df} must have all mandatory columns.',
+                 i = '{.val {mandatory_columns[is_missing]}} {?is/are} missing.'
+        )
         looks_like_quitte <- FALSE
     }
 
-    if ('period' %in% colnames(df)) {
-        class_period <- class(df[['period']])
-        if (!any(class_period %in% c('integer', 'POSIXct'))) {
-            class_period <- paste0('`', class_period, '`')
-            verbose(pluralize(
-                'Column `period` must be of class "integer" or "POSIXct", but ',
-                'has class{?es} {class_period}.'))
-            looks_like_quitte <- FALSE
-        }
+    cp <- class(df[['period']])
+    if ('period' %in% colnames(df) && !cp %in% c('integer', 'POSIXct')) {
+        msg <- c(msg,
+                 paste('Column {.field period} must be of class',
+                       '{.cls integer} or {.cls POSIXct}.'),
+                 i = 'It is of class {.cls {cp}}.')
+        looks_like_quitte <- FALSE
     }
 
-    if ('value' %in% colnames(df)) {
-        if (!is.numeric(df[['value']])) {
-            verbose('Column `value` must be of type "numeric".')
-            looks_like_quitte <- FALSE
-        }
+    if ('value' %in% colnames(df) && !is.numeric(df[['value']])) {
+        msg <- c(msg,
+                 'Column {.field value} must be of class {.cls numeric}.',
+                 i = 'It is of class {.cls {class(df[["value"]])}}.')
+        looks_like_quitte <- FALSE
     }
 
+    if (!looks_like_quitte) {
+        verbose(msg)
+    }
     return(looks_like_quitte)
 }
