52689f41d05182ce192bced9b7de9796a615acb7
Tonn Rüter <tonnru@pik-potsdam.de>
2026-09-07 19:29:37 +0200

    Rm'd `gdxrrw` read backend
    - Deleted `init_gdxrrw()` & `.read.gdx_gdxrrw()`
    - `read.gdx()` now only uses `gamstransfer` only, collapsed into a single function (no more backend dispatch or `quitte_force_gamstransfer` option)
    - Drop `gdxrrw` from `DESCRIPTION Suggests`, regenerate `man/read.gdx.Rd` & flatten `test-read.gdx.R` (the two-backend wrapper is no longer needed)

--- R/read.gdx.R
+++ R/read.gdx.R
@@ -1,19 +1,14 @@
 #' Read item from `.gdx` file as quitte data frame
 #'
-#' `read.gdx()` is a wrapper function for either [`gdxrrw::rgdx()`] or
-#' [`gamstransfer::readGDX()`] that returns a quitte data frame.
-#'
-#' `read.gdx()` will use [`gdxrrw::rgdx()`] if [gdxrrw](gdxrrw-package) is
-#' installed and the option `quitte_force_gamstransfer` is not `TRUE`, otherwise
-#' it will use [`gamstransfer::readGDX()`].
+#' `read.gdx()` is a wrapper function for [`gamstransfer::readGDX()`] that
+#' returns a quitte data frame.
 #'
 #' @param gdxName Path to a `.gdx` file.
 #' @param requestList.name Name of the item to read.
-#' @param fields Fields to read from variables and equations.  When using
-#'     [gdxrrw](gdxrrw-package), any of `l`, `m`, `lo`, `up`, `s`.  When using
-#'     using [gamstransfer](gamstransfer-package), `level`, `marginal`, `lower`,
-#'     `upper`, and `scale` are understood as well.  `all` will return all
-#'     fields.  Ignored when reading sets or parameters.
+#' @param fields Fields to read from variables and equations.  Any of `l`, `m`,
+#'     `lo`, `up`, `s` (or the long forms `level`, `marginal`, `lower`, `upper`,
+#'     and `scale`).  `all` will return all fields.  Ignored when reading sets or
+#'     parameters.
 #' @param colNames String vector of column names to override dimension and field
 #'     names.
 #' @param factors Deprecated.  Do not use any more.
@@ -35,136 +30,6 @@ read.gdx <- function(gdxName, requestList.name, fields = "l", colNames = NULL,
                        details = 'Please do not use the argument anymore.')
     }
 
-    # Check if gdxrrw package is installed
-    no_gdx_package <- !any(c('gdxrrw', 'gamstransfer') %in%
-                               .packages(all.available = TRUE))
-    if (no_gdx_package) {
-        stop('Neither `gdxrrw` nor `gamstransfer` package is installed.\n',
-             'See https://www.gams.com/latest/docs/API_R_GAMSTRANSFER.html')
-    }
-
-    # use gdxrrw unless gamstransfer is forced or gdxrrw is not installed
-    use_gdxrrw <- !any(getOption('quitte_force_gamstransfer', default = FALSE),
-                       !'gdxrrw' %in% .packages(all.available = TRUE))
-
-    if (use_gdxrrw) {
-        .read.gdx_gdxrrw(gdxName, requestList.name, fields, colNames, squeeze)
-    }
-    else {
-        .read.gdx_gamstransfer(gdxName, requestList.name, fields, colNames,
-                               squeeze)
-    }
-}
-
-init_gdxrrw <- function() {
-    if (!(done <- gdxrrw::igdx(silent = TRUE, returnStr = FALSE))) {
-        if ("Windows" == getElement(Sys.info(), "sysname")) {
-            path <- strsplit(Sys.getenv("PATH"), ";")[[1]]
-            path <- grep("gams", path, value = TRUE, ignore.case = TRUE)
-            path <- grep("%", path, value = TRUE, invert = TRUE)
-
-            for (p in path)
-                if (done <- gdxrrw::igdx(p, silent = TRUE))
-                    break
-        }
-        else {
-            suppressWarnings(
-                s <- system("which gams | xargs dirname", intern = TRUE,
-                            ignore.stderr = TRUE))
-            if (0 != length(s)) {
-                done <- gdxrrw::igdx(s, silent = TRUE)
-            }
-            else {
-                done <- FALSE
-            }
-        }
-    }
-    return(done)
-}
-
-.read.gdx_gdxrrw <- function(gdxName, requestList.name, fields, colNames,
-                             squeeze) {
-    if (!init_gdxrrw()) {
-        stop("Could not load gdx libraries")
-    }
-
-    gdxName <- path.expand(gdxName)
-
-    # if reading variable or equation, read specific fields (e.g. "lo", "m")
-    info <- gdxrrw::gdxInfo(gdxName, dump = FALSE, returnList = TRUE)
-    read.fields <- tolower(requestList.name) %in% tolower(c(info$variables,
-                                                            info$equations))
-
-    # read the first (or only) field
-    if (read.fields) {
-        requestList <- list(name = requestList.name, field = fields[[1]])
-        item <- gdxrrw::rgdx(gdxName, requestList, squeeze = squeeze)
-    }
-    else {
-        requestList <- list(name = requestList.name)
-        item <- gdxrrw::rgdx(gdxName, requestList, squeeze = squeeze)
-    }
-
-    # if item is a scalar, return a named vector
-    if (0 == item$dim) {
-        data <- as.vector(item$val)
-        names(data) <- requestList.name
-        return(data)
-    }
-
-    # convert dimension info
-    data <- list()
-    for (d in 1:(item$dim)) {
-        val <- item$val[,d]
-        uel <- item$uels[[d]]
-
-        if (all(grepl("^[0-9]+$", uel)))
-            uel <- as.numeric(uel)
-
-        data[[d]] <- c(uel[val])
-    }
-
-    # add first (or only) field
-    if (dim(item$val)[[2]] > d) {
-        d <- d + 1
-        data[[d]] <- c(item$val[,d])
-    }
-
-    # read additional fields
-    if (read.fields) {
-        for (field in fields[-1]) {
-            d <- d + 1
-            requestList <- list(name = requestList.name, field = field)
-            item <- gdxrrw::rgdx(gdxName, requestList, squeeze = squeeze)
-            data[[d]] <- c(item$val[,item$dim + 1])
-        }
-    }
-
-    if (read.fields) {
-        field.names <- sub("^l$", "value", fields)
-    }
-    else {
-        field.names <- "value"
-    }
-
-    if (is.null(colNames)) {
-        if (length(data) > length(item$domains)) {
-            names(data) <- c(item$domains, field.names)
-        } else {
-            names(data) <- c(item$domains)
-        }
-    }
-    else {
-        names(data) <- colNames
-    }
-
-    data <- as_tibble(data.frame(data))
-
-    return(data)
-}
-
-.read.gdx_gamstransfer <- function(gdxName, requestList.name, fields, colNames,
-                                   squeeze) {
     # functions ----
     is.Alias <- function(d) {
         'Alias' == d[['class']]
--- tests/testthat/test-read.gdx.R
+++ tests/testthat/test-read.gdx.R
@@ -1,271 +1,236 @@
-# define tests ----
-# all these tests a wrapped in a function so that they can be run with different
-# options below
-`test-read.gdx` <- function()
-{
-    gdx <- test_path('test_data/test.gdx')
+# read.gdx() has a single (gamstransfer) backend, so the tests run directly.
 
-    info <- paste('`quitte_force_gamstransfer` =',
-                  options('quitte_force_gamstransfer'))
+gdx <- test_path('test_data/test.gdx')
 
-    identical_tibble <- function(x, y) {
-        a <- all.equal(x, y)
-        b <- all.equal(attributes(x)[sort(names(attributes(x)))],
-                       attributes(y)[sort(names(attributes(y)))])
-
-        if (!isTRUE(a)) {
-            return(a)
-        }
-        else if (!isTRUE(b)) {
-            return(b)
-        }
-        else {
-            return(TRUE)
-        }
-    }
-
-    ## sets ----
-    test_that(
-        'read.gdx() reads sets correctly',
-        {
-            x <- read.gdx(gdx, 'set_d1_lower')
-
-            expect_true(tibble::is_tibble(x), info = info)
-            expect_identical('foo', colnames(x), info = info)
-            expect_identical(c('foo' = 'character'), sapply(x, class),
-                             info = info)
-            expect_equal(length(set_d1_lower), nrow(x), info = info)
-        })
-
-    test_that(
-        'read.gdx() reads two-dimensional sets correctly',
-        {
-            x <- read.gdx(gdx, 'set_d2_alias')
-
-            expect_true(tibble::is_tibble(x), info = info)
-
-            expect_identical(c('set_d1_UPPER', 'set_d1_lower'), colnames(x),
-                             info = info)
-
-            expect_identical(
-                sapply(x, class),
-                c('set_d1_UPPER' = 'character', 'set_d1_lower' = 'character'),
-                info = info)
-
-            expect_equal(prod(lengths(list(set_d1_UPPER, set_d1_lower))),
-                         nrow(x),
-                         info = info)
-        })
-
-    test_that(
-        'read.gdx() reads set aliases correctly',
-        {
-            x <- read.gdx(gdx, 'set_d2')
-            y <- read.gdx(gdx, 'set_d2_alias')
-
-            expect_identical(x, y, info = info)
-        })
-
-    test_that(
-        'read.gdx() reads two-dimensional sets with identical defining sets',
-        {
-            x <- read.gdx(gdx, 'set_d2_identical')
-
-            expect_identical(x, set_d2_identical)
-        })
-
-
-    ## parameters ----
-    test_that(
-        'read.gdx() reads scalars correctly',
-        {
-            x <- read.gdx(gdx, 'parameter_d0')
-
-            expect_equal(object = x,
-                         expected = c('parameter_d0' = parameter_d0),
-                         info = info)
-        })
-
-    test_that(
-        'read.gdx() reads parameters correctly',
-        {
-            x <- read.gdx(gdx, 'parameter_d1')
-
-            expect_true(identical_tibble(x, parameter_d1), info = info)
-        })
-
-    test_that(
-        'read.gdx() reads two-dimensional parameters correctly',
-        {
-            x <- read.gdx(gdx, 'parameter_d2')
-
-            expect_true(identical_tibble(x, parameter_d2), info = info)
-        })
-
-    test_that(
-        'read.gdx() reads parameters with no defined values correctly',
-        {
-            x <- read.gdx(gdx, 'parameter_d2_0', squeeze = FALSE)
-
-            expect_true(identical_tibble(x, parameter_d2_0), info = info)
-        })
-
-    test_that(
-        'read.gdx() squeezes out stored zeros and EPS by default',
-        {
-            x <- read.gdx(gdx, 'parameter_d1_squeeze')
-
-            expect_true(identical_tibble(x, parameter_d1_squeeze_squeezed),
-                        info = info)
-        })
-
-    test_that(
-        'read.gdx() keeps stored zeros and EPS when squeeze = FALSE',
-        {
-            x <- read.gdx(gdx, 'parameter_d1_squeeze', squeeze = FALSE)
-
-            expect_true(identical_tibble(x, parameter_d1_squeeze), info = info)
-        })
-
-    test_that(
-        'read.gdx() coerces integer-like set elements to numeric',
-        {
-            x <- read.gdx(gdx, 'parameter_years')
-
-            expect_identical(class(x[['set_years']]), 'numeric', info = info)
-            expect_true(identical_tibble(x, parameter_years), info = info)
-        })
-
-    test_that(
-        'read.gdx() preserves +Inf, -Inf and NA special values',
-        {
-            x <- read.gdx(gdx, 'parameter_special')
-
-            expect_true(identical_tibble(x, parameter_special), info = info)
-        })
-
-    ## variables ----
-    test_that(
-        'read.gdx() reads scalar variables correctly',
-        {
-            x <- read.gdx(gdx, 'variable_d0')
-
-            expect_equal(x,
-                         c('variable_d0' = variable_d0[['level']]),
-                         info = info)
-        })
-
-    test_that(
-        'read.gdx() reads one-dimensional variables correctly',
-        {
-            x <- read.gdx(gdx, 'variable_d1')
-
-            expect_true(
-                identical_tibble(
-                    x,
-                    variable_d1 %>%
-                        select('set_d1_UPPER', 'value' = 'level')),
-                info = info)
-
-            x <- read.gdx(gdx, 'variable_d1',
-                          fields = c('l', 'm', 'lo', 'up', 's'),
-                          colNames = c('set_d1_UPPER', 'level', 'marginal',
-                                       'lower', 'upper', 'scale'),
-                          squeeze = FALSE)
-
-            expect_true(identical_tibble(x, variable_d1), info = info)
-        })
-
-    test_that(
-        'read.gdx() reads two-dimensional variables correctly',
-        {
-            x <- read.gdx(gdx, 'variable_d2')
-
-            expect_true(
-                identical_tibble(x,
-                                 variable_d2 %>%
-                                     select('set_d1_UPPER', 'set_d1_lower',
-                                            'value' = 'level')),
-                info = info)
-
-            x <- read.gdx(gdx, 'variable_d2',
-                          fields = c('l', 'm', 'lo', 'up', 's'),
-                          colNames = c('set_d1_UPPER', 'set_d1_lower',
-                                       'level', 'marginal', 'lower', 'upper',
-                                       'scale'),
-                          squeeze = FALSE)
-
-            expect_true(identical_tibble(x, variable_d2), info = info)
-        })
-
-    ## equations ----
-    test_that(
-        'read.gdx() reads scalar equations correctly',
-        {
-            x <- read.gdx(gdx, 'equation_d0')
-
-            expect_identical(x,
-                             c('equation_d0' = equation_d0[['level']]),
-                             info = info)
-        })
-
-    test_that(
-        'read.gdx() reads one-dimensional equations correctly',
-        {
-            x <- read.gdx(gdx, 'equation_d1')
-
-            expect_true(
-                identical_tibble(
-                    x,
-                    equation_d1 %>%
-                        select('set_d1_UPPER', 'value' = 'level')),
-                info = info)
-
-            x <- read.gdx(gdx, 'equation_d1',
-                          fields = c('l', 'm', 'lo', 'up', 's'),
-                          colNames = c('set_d1_UPPER', 'level', 'marginal',
-                                       'lower', 'upper', 'scale'),
-                          squeeze = FALSE)
-
-            expect_true(identical_tibble(x, equation_d1), info = info)
-        })
-
-    test_that(
-        'read.gdx() reads two-dimensional equations correctly',
-        {
-            x <- read.gdx(gdx, 'equation_d2')
-
-            expect_true(
-                identical_tibble(x,
-                                 equation_d2 %>%
-                                     select('set_d1_UPPER', 'set_d1_lower',
-                                            'value' = 'level')),
-                info = info)
-
-            x <- read.gdx(gdx, 'equation_d2',
-                          fields = c('l', 'm', 'lo', 'up', 's'),
-                          colNames = c('set_d1_UPPER', 'set_d1_lower',
-                                       'level', 'marginal', 'lower', 'upper',
-                                       'scale'),
-                          squeeze = FALSE)
-
-            expect_true(identical_tibble(x, equation_d2), info = info)
-        })
-}
-
-# run tests ----
 # set up test data to compare to
 source(test_path('test_data/make_test_data.R'))
 
-if (   'gdxrrw' %in% .packages(all.available = TRUE)
-    && init_gdxrrw()) {
-    withr::with_options(
-        new = list('quitte_force_gamstransfer' = FALSE),
-        `test-read.gdx`())
+identical_tibble <- function(x, y) {
+    a <- all.equal(x, y)
+    b <- all.equal(attributes(x)[sort(names(attributes(x)))],
+                   attributes(y)[sort(names(attributes(y)))])
+
+    if (!isTRUE(a)) {
+        return(a)
+    }
+    else if (!isTRUE(b)) {
+        return(b)
+    }
+    else {
+        return(TRUE)
+    }
 }
 
-if ('gamstransfer' %in% .packages(all.available = TRUE)) {
-    withr::with_options(
-        new = list('quitte_force_gamstransfer' = TRUE),
-        `test-read.gdx`())
-}
+## sets ----
+test_that(
+    'read.gdx() reads sets correctly',
+    {
+        x <- read.gdx(gdx, 'set_d1_lower')
+
+        expect_true(tibble::is_tibble(x))
+        expect_identical('foo', colnames(x))
+        expect_identical(c('foo' = 'character'), sapply(x, class))
+        expect_equal(length(set_d1_lower), nrow(x))
+    })
+
+test_that(
+    'read.gdx() reads two-dimensional sets correctly',
+    {
+        x <- read.gdx(gdx, 'set_d2_alias')
+
+        expect_true(tibble::is_tibble(x))
+
+        expect_identical(c('set_d1_UPPER', 'set_d1_lower'), colnames(x))
+
+        expect_identical(
+            sapply(x, class),
+            c('set_d1_UPPER' = 'character', 'set_d1_lower' = 'character'))
+
+        expect_equal(prod(lengths(list(set_d1_UPPER, set_d1_lower))),
+                     nrow(x))
+    })
+
+test_that(
+    'read.gdx() reads set aliases correctly',
+    {
+        x <- read.gdx(gdx, 'set_d2')
+        y <- read.gdx(gdx, 'set_d2_alias')
+
+        expect_identical(x, y)
+    })
+
+test_that(
+    'read.gdx() reads two-dimensional sets with identical defining sets',
+    {
+        x <- read.gdx(gdx, 'set_d2_identical')
+
+        expect_identical(x, set_d2_identical)
+    })
+
+
+## parameters ----
+test_that(
+    'read.gdx() reads scalars correctly',
+    {
+        x <- read.gdx(gdx, 'parameter_d0')
+
+        expect_equal(object = x,
+                     expected = c('parameter_d0' = parameter_d0))
+    })
+
+test_that(
+    'read.gdx() reads parameters correctly',
+    {
+        x <- read.gdx(gdx, 'parameter_d1')
+
+        expect_true(identical_tibble(x, parameter_d1))
+    })
+
+test_that(
+    'read.gdx() reads two-dimensional parameters correctly',
+    {
+        x <- read.gdx(gdx, 'parameter_d2')
+
+        expect_true(identical_tibble(x, parameter_d2))
+    })
+
+test_that(
+    'read.gdx() reads parameters with no defined values correctly',
+    {
+        x <- read.gdx(gdx, 'parameter_d2_0', squeeze = FALSE)
+
+        expect_true(identical_tibble(x, parameter_d2_0))
+    })
+
+test_that(
+    'read.gdx() squeezes out stored zeros and EPS by default',
+    {
+        x <- read.gdx(gdx, 'parameter_d1_squeeze')
+
+        expect_true(identical_tibble(x, parameter_d1_squeeze_squeezed))
+    })
+
+test_that(
+    'read.gdx() keeps stored zeros and EPS when squeeze = FALSE',
+    {
+        x <- read.gdx(gdx, 'parameter_d1_squeeze', squeeze = FALSE)
+
+        expect_true(identical_tibble(x, parameter_d1_squeeze))
+    })
+
+test_that(
+    'read.gdx() coerces integer-like set elements to numeric',
+    {
+        x <- read.gdx(gdx, 'parameter_years')
+
+        expect_identical(class(x[['set_years']]), 'numeric')
+        expect_true(identical_tibble(x, parameter_years))
+    })
+
+test_that(
+    'read.gdx() preserves +Inf, -Inf and NA special values',
+    {
+        x <- read.gdx(gdx, 'parameter_special')
+
+        expect_true(identical_tibble(x, parameter_special))
+    })
+
+## variables ----
+test_that(
+    'read.gdx() reads scalar variables correctly',
+    {
+        x <- read.gdx(gdx, 'variable_d0')
+
+        expect_equal(x, c('variable_d0' = variable_d0[['level']]))
+    })
+
+test_that(
+    'read.gdx() reads one-dimensional variables correctly',
+    {
+        x <- read.gdx(gdx, 'variable_d1')
+
+        expect_true(
+            identical_tibble(
+                x,
+                variable_d1 %>%
+                    select('set_d1_UPPER', 'value' = 'level')))
+
+        x <- read.gdx(gdx, 'variable_d1',
+                      fields = c('l', 'm', 'lo', 'up', 's'),
+                      colNames = c('set_d1_UPPER', 'level', 'marginal',
+                                   'lower', 'upper', 'scale'),
+                      squeeze = FALSE)
+
+        expect_true(identical_tibble(x, variable_d1))
+    })
+
+test_that(
+    'read.gdx() reads two-dimensional variables correctly',
+    {
+        x <- read.gdx(gdx, 'variable_d2')
+
+        expect_true(
+            identical_tibble(x,
+                             variable_d2 %>%
+                                 select('set_d1_UPPER', 'set_d1_lower',
+                                        'value' = 'level')))
+
+        x <- read.gdx(gdx, 'variable_d2',
+                      fields = c('l', 'm', 'lo', 'up', 's'),
+                      colNames = c('set_d1_UPPER', 'set_d1_lower',
+                                   'level', 'marginal', 'lower', 'upper',
+                                   'scale'),
+                      squeeze = FALSE)
+
+        expect_true(identical_tibble(x, variable_d2))
+    })
+
+## equations ----
+test_that(
+    'read.gdx() reads scalar equations correctly',
+    {
+        x <- read.gdx(gdx, 'equation_d0')
+
+        expect_identical(x, c('equation_d0' = equation_d0[['level']]))
+    })
+
+test_that(
+    'read.gdx() reads one-dimensional equations correctly',
+    {
+        x <- read.gdx(gdx, 'equation_d1')
+
+        expect_true(
+            identical_tibble(
+                x,
+                equation_d1 %>%
+                    select('set_d1_UPPER', 'value' = 'level')))
+
+        x <- read.gdx(gdx, 'equation_d1',
+                      fields = c('l', 'm', 'lo', 'up', 's'),
+                      colNames = c('set_d1_UPPER', 'level', 'marginal',
+                                   'lower', 'upper', 'scale'),
+                      squeeze = FALSE)
+
+        expect_true(identical_tibble(x, equation_d1))
+    })
+
+test_that(
+    'read.gdx() reads two-dimensional equations correctly',
+    {
+        x <- read.gdx(gdx, 'equation_d2')
+
+        expect_true(
+            identical_tibble(x,
+                             equation_d2 %>%
+                                 select('set_d1_UPPER', 'set_d1_lower',
+                                        'value' = 'level')))
+
+        x <- read.gdx(gdx, 'equation_d2',
+                      fields = c('l', 'm', 'lo', 'up', 's'),
+                      colNames = c('set_d1_UPPER', 'set_d1_lower',
+                                   'level', 'marginal', 'lower', 'upper',
+                                   'scale'),
+                      squeeze = FALSE)
+
+        expect_true(identical_tibble(x, equation_d2))
+    })
